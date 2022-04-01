4e1eb60567093515dcf75a356f09efaec796d8a3
Sai Kumar <devulapellisaikumar@gmail.com>
2022-04-01 13:22:44 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,21 @@
+import os
+from util import get_spark_session
+from read import from_files
+from process import transform
+from write import to_files
+
+def main():
+    env = os.environ.get('ENVIRON')
+    src_dir = os.environ.get('SRC_DIR')
+    file_pattern = f"{os.environ.get('SRC_FILE_PATTERN')}-*"
+    src_file_format = os.environ.get('SRC_FILE_FORMAT')
+    tgt_dir = os.environ.get('TGT_DIR')
+    tgt_file_format = os.environ.get('TGT_FILE_FORMAT')
+    spark = get_spark_session(env, 'GitHub Activity - Reading Data')
+    df = from_files(spark, src_dir, file_pattern, src_file_format)
+    df_transformed = transform(df)
+    to_files(df_transformed, tgt_dir, tgt_file_format)
+
+
+if __name__ == '__main__':
+    main()
